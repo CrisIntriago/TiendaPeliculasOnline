d1854b51c218cd7e9879fa3ad99af61bbc92f4cf
fredtenesaca <101230553+fredtenesaca@users.noreply.github.com>
2023-11-29 10:43:31 -0500

    Shared components e Index de Guía 14

--- mpa/src/app/app.component.ts
+++ mpa/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from './shared/topbar/topbar.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { IndexComponent } from './pages/index/index.component';
@@ -8,7 +9,7 @@ import { IndexComponent } from './pages/index/index.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, IndexComponent],
+  imports: [CommonModule, RouterOutlet, TopbarComponent, NavbarComponent, FooterComponent, IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
