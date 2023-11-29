85db4c9a261d1c6a16260fbe20ee655a09671c2c
fredtenesaca <101230553+fredtenesaca@users.noreply.github.com>
2023-11-29 10:47:06 -0500

    Back To Top agregado en shared

--- mpa/src/app/app.component.ts
+++ mpa/src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { TopbarComponent } from './shared/topbar/topbar.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { BacktotopComponent } from './shared/backtotop/backtotop.component';
 import { IndexComponent } from './pages/index/index.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TopbarComponent, NavbarComponent, FooterComponent, IndexComponent],
+  imports: [CommonModule, RouterOutlet, TopbarComponent, NavbarComponent, FooterComponent, BacktotopComponent, IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ mpa/src/app/shared/backtotop/backtotop.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-backtotop',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './backtotop.component.html',
+  styleUrl: './backtotop.component.css'
+})
+export class BacktotopComponent {
+
+}
