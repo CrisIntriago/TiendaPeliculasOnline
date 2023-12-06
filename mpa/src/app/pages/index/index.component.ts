b4e4c347bcc5dd14741ba50e4110acf1b194fd26
Christopher Acosta <chriacos@espol.edu.ec>
2023-12-05 19:41:59 -0500

    Guia 15 v2

--- mpa/src/app/pages/index/index.component.ts
+++ mpa/src/app/pages/index/index.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HttpClientModule } from '@angular/common/http';
+//Importación de la interfaz
+import { Pelicula } from '../../interfaces/pelicula';
+//Importación del servicio
+import { DatospeliculaService } from '../../providers/datospelicula.service'
+
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
+  providers: [DatospeliculaService],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
-export class IndexComponent {
 
+export class IndexComponent {
+  //Atributo con el tipo de dato de la interfaz
+  public data: Pelicula[] = [];
+
+  //Inyección de dependencia del servicio
+  constructor(private dataProvider: DatospeliculaService) { }
+
+  //Ejecución de la petición y suscripción de la respuesta
+
+  ngOnInit() {
+    this.dataProvider.getResponse().subscribe((response) => {
+      let dataArray = (response as Pelicula[]);
+      this.data = dataArray.slice(0, 10);
+    })
+  }
 }
